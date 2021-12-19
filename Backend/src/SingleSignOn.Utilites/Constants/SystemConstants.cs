b25ea89d387a852727cf283c86ac8c0a84f6904c
phandinhtri98 <triphan98it@outlook.com>
2021-12-19 22:33:23 +0700

    up code

---
+++ Backend/src/SingleSignOn.Utilites/Constants/SystemConstants.cs
@@ -0,0 +1,39 @@
+﻿namespace SingleSignOn.Utilites.Constants
+{
+    public static class SystemConstants
+    {
+        public static class Roles
+        {
+            public const string Admin = "Admin";
+        }
+        public static class Permission
+        {
+            public const string Type = "Permissions";
+        }
+        public static class View
+        {
+            public const string Type = "VIEW";
+        }
+        public static class Create
+        {
+            public const string Type = "CREATE";
+        }
+        public static class Update
+        {
+            public const string Type = "UPDATE";
+        }
+        public static class Delete
+        {
+            public const string Type = "DELETE";
+        }
+
+        public static class True
+        {
+            public const string Type = "true";
+        }
+        public static class False
+        {
+            public const string Type = "false";
+        }
+    }
+}
